704ad95afb7be20db09d27d37b2137f849b40360
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-06-19 19:25:24 +0200

    Dodaj pliki projektów.

---
+++ BMI/Program.cs
@@ -0,0 +1,16 @@
+﻿double waga;
+double wzrost;
+double bmi;
+Console.WriteLine("Podaj wage(w kg):");
+waga = double.Parse(Console.ReadLine());
+Console.WriteLine("Podaj wzorst(w metrach):");
+wzrost = double.Parse(Console.ReadLine());
+bmi = (waga / (wzrost * wzrost));
+bmi = Math.Round(bmi, 2);
+Console.WriteLine("Twoje BMI wynosi:{0} ", bmi);
+if (bmi <= 18.5)
+    Console.WriteLine("Zacznij jesc");
+else if (bmi >=25)
+    Console.WriteLine("Masz nadwage jebany ulancu");
+else
+    Console.WriteLine("Waga prawidlowa");
